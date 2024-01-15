a08078bbf8f9271a4b8b5f032930800a1e69b30a
tailongswayam-frinks <swayam.tailong@frinks.ai>
2024-01-15 18:44:28 +0530

    changed server.js for single image

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const {resultData, resTitles} = require('./images');
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { resultData, resTitles, baseImages } = require("./images");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,43 +9,43 @@ const server = http.createServer(app);
 app.use(cors());
 
 // SSE endpoint
-app.get('/sse', (req, res) => {
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Access-Control-Allow-Origin', '*');
+app.get("/sse", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-    // Sample array of base64-encoded images
+  // Sample array of base64-encoded images
 
-    let currentIndex = 0;
+  let currentIndex = 0;
 
-    // Send initial image
+  // Send initial image
+  // console.log('sending data: ', resultData[currentIndex]);
+  const data = {
+    image: baseImages[currentIndex],
+    pass: !!currentIndex,
+  };
+  res.write(`data: ${JSON.stringify(data)}\n\n`);
+
+  // Send images in a circular fashion every 5 seconds
+  const intervalId = setInterval(() => {
+    currentIndex = (currentIndex + 1) % resultData.length;
     // console.log('sending data: ', resultData[currentIndex]);
     const data = {
-      'resultData': resultData[currentIndex],
-      'title': resTitles[currentIndex]
-    }
+      image: baseImages[currentIndex],
+      pass: !!currentIndex,
+    };
     res.write(`data: ${JSON.stringify(data)}\n\n`);
+  }, 2000);
 
-    // Send images in a circular fashion every 5 seconds
-    const intervalId = setInterval(() => {
-        currentIndex = (currentIndex + 1) % resultData.length;
-        // console.log('sending data: ', resultData[currentIndex]);
-        const data = {
-          'resultData': resultData[currentIndex],
-          'title': resTitles[currentIndex]
-        }
-        res.write(`data: ${JSON.stringify(data)}\n\n`);
-    }, 2000);
-
-    // Handle client disconnect
-    req.on('close', () => {
-        clearInterval(intervalId);
-        res.end();
-    });
+  // Handle client disconnect
+  req.on("close", () => {
+    clearInterval(intervalId);
+    res.end();
+  });
 });
 
 const port = 9000;
 server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
